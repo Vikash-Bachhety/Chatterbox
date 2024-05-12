e82ddc40b5da6f93073752747864890b6d6a0e7e
vikash <imvikky31@gmail.com>
2024-05-12 22:13:52 +0530

    fix bug

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({ origin: ["https://chatterbox-app.vercel.app/", "http://localhost:5173"], 
+app.use(cors({ origin: ["https://chatterbox-app.vercel.app", "http://localhost:5173"], 
 methods:["get","post"],
 credentials: true }));
 app.use(express.json());
